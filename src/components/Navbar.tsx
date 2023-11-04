87dbeff5918301eff67dca4c8d15d7194711267b
elorenzoacevedo <ezequiel-lorenzo@hotmail.com>
2023-11-04 15:15:59 -0400

    progress in inventory page design

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
-import { AppBar, Toolbar, Typography, IconButton, Box } from '@mui/material';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Box,
+  Menu,
+  MenuItem,
+  Divider,
+} from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+import { useRouter } from 'next/router';
+
 const styles = {
   appBar: {
     backgroundColor: '#0D0A9A',
@@ -19,19 +30,62 @@ const styles = {
 };
 
 const NavBar = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const router = useRouter();
+
+  const handleMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleMenuItemClick = (path: string) => {
+    router.push(path);
+    handleMenuClose();
+  };
+
   return (
     <AppBar position='static' sx={styles.appBar}>
       <Toolbar>
-        {/* Logo */}
         <Box sx={styles.logo}>
           <Image src='/poseidon_logo.png' alt='Logo' width='55' height='55' />
         </Box>
         <Typography variant='h4' sx={styles.title}>
           POSEIDON
         </Typography>
-        <IconButton edge='end' sx={styles.menuIcon} aria-label='menu'>
+        <IconButton
+          edge='end'
+          sx={styles.menuIcon}
+          aria-label='menu'
+          onClick={handleMenuOpen}
+        >
           <MenuIcon />
         </IconButton>
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleMenuClose}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+        >
+          <MenuItem onClick={() => handleMenuItemClick('/')}>Home</MenuItem>
+          <Divider />
+          <MenuItem onClick={() => handleMenuItemClick('/inventory')}>
+            Inventory
+          </MenuItem>
+          <Divider />
+          <MenuItem onClick={() => handleMenuItemClick('/billing')}>
+            Billing
+          </MenuItem>
+        </Menu>
       </Toolbar>
     </AppBar>
   );
